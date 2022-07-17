773da887a121b55288cca2fee8241db50b9fb9b6
SirMorfield <jkoers@student.codam.nl>
2022-07-17 19:07:27 +0200

    feat: actually implement proxy

--- jkoers/ch11_proxy_pattern/src/main.cpp
+++ jkoers/ch11_proxy_pattern/src/main.cpp
@@ -1,6 +1,7 @@
 #include "rpc.hpp"
 #include "transfer_types.hpp"
 #include <iostream>
+#include <map>
 #include <string>
 
 void print_error() {
@@ -8,9 +9,51 @@ void print_error() {
 	std::cout << "Usage: client <IPV4 address> <port>" << std::endl;
 }
 
-Response handler(const Request& request) {
+// compute result
+Response computer(const Request& request) {
 	const Response response(request.number * 2);
 	return response;
+}
+
+void server(uint16_t port) {
+	while (true) {
+		RPC_server<Request, Response> server(port);
+		server.respond(computer);
+		std::cout << "accepted" << std::endl;
+	}
+}
+
+std::map<int, int> cache; // TODO: user Request() and Response instead of int
+//
+Response proxy(const Request& request, RPC_client<Request, Response>& client) {
+	if (cache.find(request.number) != cache.end()) {
+		std::cout << "Cache hit" << std::endl;
+		return Response(cache[request.number]);
+	}
+
+	std::cout << "Cache miss" << std::endl;
+	Response response = client.call(request);
+
+	cache[request.number] = response.number;
+	return response;
+}
+
+void client(const std::string& host, uint16_t port) {
+	RPC_client<Request, Response> client(host, port);
+
+	while (true) {
+		Request request;
+		std::cout << "\nEnter number: " << std::ends;
+		std::cin >> request.number;
+
+		auto	 start = std::chrono::high_resolution_clock::now();
+
+		Response response = proxy(request, client);
+
+		auto	 end = std::chrono::high_resolution_clock::now();
+		auto	 duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+		std::cout << "Got response: " << response.number << " in " << duration.count() << " microseconds" << std::endl;
+	}
 }
 
 int main(int argc, const char* argv[]) {
@@ -19,16 +62,10 @@ int main(int argc, const char* argv[]) {
 		return 1;
 	}
 	else if (!strcmp(argv[1], "server") && argc == 3) {
-		while (true) {
-			RPC_server<Request, Response> server(atoi(argv[2]));
-			server.respond(handler);
-			std::cout << "accepted" << std::endl;
-		}
+		server(atoi(argv[2]));
 	}
 	else if (!strcmp(argv[1], "client") && argc == 4) {
-		RPC_client<Request, Response> client(argv[2], atoi(argv[3]));
-		Response					  response = client.call(Request(42));
-		std::cout << "got response " << response.number << std::endl;
+		client(argv[2], atoi(argv[3]));
 	}
 	else {
 		print_error();
