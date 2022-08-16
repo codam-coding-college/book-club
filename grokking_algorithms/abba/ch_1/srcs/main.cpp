a49f836976c168058421f07539d89e60617ba172
Abdoulaye <abba@student.codam.nl>
2022-08-17 00:48:31 +0200

    made a few improvement and remove the class

--- grokking_algorithms/abba/ch_1/srcs/main.cpp
+++ grokking_algorithms/abba/ch_1/srcs/main.cpp
@@ -1,12 +1,58 @@
-#include "../includes/binarySearch.hpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
 
-int main(void) {
-	std::vector<int> list;
-
-	for (size_t i = 0; i < 100; ++i) {
-		list.push_back((rand() % 200) + 1);
+template<class T>
+int binary_search(std::vector<T> list, const T item) {
+	size_t	low = 0;
+	size_t	high = list.size();
+	while (low <= high) {
+		size_t mid = ((low + high) / 2);
+		std::string guest = list.at(mid);
+		if (guest == item) {
+			return mid;
+		} else if (guest > item) {
+			std::cout << mid << " im here\n";
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
 	}
-	std::sort(list.begin(), list.end());
-	BinarySearch search;
-	std::cout << "test " << search.binary_search(list, 10);
+	return -1;
 }
+
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		std::cerr << "usage: <program> <file> <value to check>\n";
+		return 1;
+	}
+	std::vector<std::string> buffer;
+	std::ifstream is(argv[1]);
+	if (is.fail()) {
+		std::cout << "invalid file\n";
+		return 1;
+	}
+	std::string line;
+	while (is.good())
+	{
+		getline(is, line);
+		if (line.size() > 0) {
+			buffer.push_back(line);
+		}
+	}
+	std::sort(buffer.begin(), buffer.end());
+	std::string find = argv[2];
+	std::cout << "test " << binary_search(buffer, find) << std::endl;
+}
+
+
+//int main(void) {
+//	std::vector<int> list;
+//
+//	for (size_t i = 0; i < 100; ++i) {
+//		list.push_back((rand() % 200) + 1);
+//	}
+//	std::sort(list.begin(), list.end());
+//	std::cout << "test " << binary_search(list, 10);
+//}
