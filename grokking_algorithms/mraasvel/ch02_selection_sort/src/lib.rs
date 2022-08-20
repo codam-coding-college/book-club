3fbb12d250b6e157e6123d27186ee9d36a0d5a71
mraasvel <mraasvel@student.codam.nl>
2022-08-20 18:23:49 +0200

    selection sort

---
+++ grokking_algorithms/mraasvel/ch02_selection_sort/src/lib.rs
@@ -0,0 +1,45 @@
+use std::cmp::Ordering;
+
+fn find_smallest<T>(table: &[T]) -> usize
+where
+    T: PartialOrd,
+{
+    table
+        .iter()
+        .enumerate()
+        .min_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap_or(Ordering::Equal))
+        .map(|(index, _)| index)
+        .unwrap_or(0)
+}
+
+pub fn selection_sort<T>(table: &mut [T])
+where
+    T: PartialOrd,
+{
+    if table.is_empty() {
+        return;
+    }
+    for index in 0..table.len() - 1 {
+        let smallest = index + find_smallest(&table[index..]);
+        table.swap(index, smallest);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn is_sorted<T>(data: &[T]) -> bool
+    where
+        T: Ord,
+    {
+        data.windows(2).all(|w| w[0] <= w[1])
+    }
+
+    #[test]
+    fn test_selection_sort() {
+        let mut table = [5, 2, 3, 1, 4];
+        selection_sort(&mut table);
+        assert!(is_sorted(&table));
+    }
+}
