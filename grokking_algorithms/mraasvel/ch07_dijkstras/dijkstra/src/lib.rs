d1e2ff2faf5cd3a516d48a284e7b82b95298c6c1
mraasvel <mraasvel@student.codam.nl>
2022-09-19 21:41:12 +0200

    dijkstra cleanup

--- grokking_algorithms/mraasvel/ch07_dijkstras/dijkstra/src/lib.rs
+++ grokking_algorithms/mraasvel/ch07_dijkstras/dijkstra/src/lib.rs
@@ -1,21 +1,75 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
-pub type Edge<'a> = (&'a str, usize);
+pub type Edge<T> = (T, usize);
 
 #[derive(Debug)]
-struct NodeData<'a> {
+struct NodeData<T> {
     cost: usize,
-    previous: Option<&'a str>,
+    previous: Option<T>,
 }
 
-type NodeDataMap<'a, 'b> = HashMap<&'a str, NodeData<'b>>;
+type NodeDataMap<T> = HashMap<T, NodeData<T>>;
 
-pub fn solve<'a>(
-    start: &'a str,
-    goal: &'a str,
-    graph: &HashMap<&'a str, Vec<Edge<'a>>>,
-) -> Vec<&'a str> {
-	let mut nodes = NodeDataMap::<'a, 'a>::new();
+fn find_lowest_cost_node<'a, T>(nodes: &'a NodeDataMap<T>, processed: &HashSet<T>) -> (&'a T, usize)
+where
+    T: Hash + Eq + Clone,
+{
+    nodes
+        .iter()
+        .filter(|(name, _)| !processed.contains(name))
+        .min_by_key(|(_, node)| node.cost)
+        .map(|(name, node)| (name, node.cost))
+        .unwrap()
+}
+
+fn construct_history<T>(goal: T, nodes: &NodeDataMap<T>) -> Vec<T>
+where
+    T: Hash + Eq + Clone,
+{
+    let mut node = nodes.get(&goal).unwrap();
+    let mut history = vec![goal];
+    while let Some(prev) = node.previous.as_ref() {
+        history.push(prev.clone());
+        node = nodes.get(prev).unwrap();
+    }
+    history.reverse();
+    history
+}
+
+fn update_connections<T>(
+    name: T,
+    cost: usize,
+    nodes: &mut NodeDataMap<T>,
+    graph: &HashMap<T, Vec<Edge<T>>>,
+) where
+    T: Hash + Eq + Clone,
+{
+    for (edge, weight) in graph.get(&name).unwrap() {
+        let edge = edge.clone();
+        let new_cost = weight + cost;
+        nodes
+            .entry(edge)
+            .and_modify(|node| {
+                if new_cost < node.cost {
+                    node.cost = new_cost;
+                    node.previous = Some(name.clone());
+                }
+            })
+            .or_insert_with(|| NodeData {
+                cost: new_cost,
+                previous: Some(name.clone()),
+            });
+    }
+}
+
+pub fn solve<T>(start: T, goal: T, graph: &HashMap<T, Vec<Edge<T>>>) -> Vec<T>
+where
+    T: Hash + Eq + Clone,
+{
+    let mut nodes = NodeDataMap::<T>::new();
     nodes.insert(
         start,
         NodeData {
@@ -25,40 +79,12 @@ pub fn solve<'a>(
     );
     let mut processed = HashSet::new();
     loop {
-        // get lowest cost node
-        let (&name, &NodeData { cost, .. }) = nodes
-            .iter()
-            .filter(|(&name, _)| !processed.contains(name))
-            .min_by_key(|(_, node)| node.cost)
-            .unwrap();
-        if name == goal {
+        let (name, cost) = find_lowest_cost_node(&nodes, &processed);
+        if name == &goal {
             break;
         }
-        // update neighbours as appropriate
-        for (edge, weight) in graph.get(name).unwrap() {
-            let new_cost = weight + cost;
-            nodes
-                .entry(edge)
-                .and_modify(|node| {
-                    if new_cost < node.cost {
-                        node.cost = new_cost;
-                        node.previous = Some(name);
-                    }
-                })
-                .or_insert_with(|| NodeData {
-                    cost: new_cost,
-                    previous: Some(name),
-                });
-        }
-        processed.insert(name);
+        processed.insert(name.clone());
+        update_connections(name.clone(), cost, &mut nodes, graph);
     }
-    // construct history
-    let mut result = vec![goal];
-    let mut node = nodes.get(goal).unwrap();
-    while let Some(&prev) = node.previous.as_ref() {
-        result.push(prev);
-        node = nodes.get(prev).unwrap();
-    }
-    result.reverse();
-    result
+    construct_history(goal, &nodes)
 }
